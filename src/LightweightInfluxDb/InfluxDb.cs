cb872ef7f608170b5c2ddbc0c25a768bc2314558
Matt Howlett <matt@mhowlett.com>
2015-07-04 23:19:41 +0700

    idiomatic dir structure.

---
+++ src/LightweightInfluxDb/InfluxDb.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LightweightInfluxDb
+{
+    public class InfluxDb
+    {
+        private string _url;
+        private string _credentials;
+
+        public InfluxDb(string url, string db, string user, string password)
+        {
+            _url = url;
+            if (!_url.EndsWith("/")) { _url = _url + "/"; }
+            _credentials = "?db=" + db + "&u=" + user + "&p=" + password;
+        }
+
+        private static string SerializeWriteData(ISeriesPoint data)
+        {
+            if (data.Fields.Count != data.Values.Count)
+            {
+                throw new Exception("Invalid field series point - number of fields does not match number of values.");
+            }
+            var result = data.Name;
+            if (data.Tags.Count > 0)
+            {
+                foreach (var kvp in data.Tags)
+                {
+                    result += "," + kvp.Key + "=" + kvp.Value;
+                }
+            }
+            result += " ";
+            for (int i = 0; i < data.Fields.Count; ++i)
+            {
+                if (i != 0) { result += ","; }
+                string strRep;
+                if (data.Values[i].GetType() == typeof(double))
+                {
+                    strRep = ((double)data.Values[i]).ToString(".0##############");
+                }
+                else if (data.Values[i].GetType() == typeof(float))
+                {
+                    strRep = ((float)data.Values[i]).ToString(".0######");
+                }
+                else
+                {
+                    strRep = "" + data.Values[i];
+                }
+                result += data.Fields[i] + "=" + strRep;
+            }
+            return result;
+        }
+
+        public Task Write(List<ISeriesPoint> data)
+        {
+            string s = "";
+            foreach (var d in data)
+            {
+                s += SerializeWriteData(d) + "\n";
+            }
+            return Write(s);
+        }
+
+        public Task Write(ISeriesPoint data)
+        {
+            return Write(SerializeWriteData(data));
+        }
+
+        private async Task Write(string data)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                HttpResponseMessage response = await client.PostAsync("write" + _credentials, new StringContent(data));
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("unable to write series point data.");
+                }
+            }
+        }
+
+        public class QuerySeries
+        {
+            public string Name;
+            public List<string> Columns;
+            public List<List<object>> Values;
+        }
+
+        public async Task<List<List<object>>> QuerySingleSeries(string query)
+        {
+            var queryString = Uri.EscapeDataString(query);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var url = "query" + _credentials + "&q=" + queryString;
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("unable to write series point data.");
+                }
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var sr = new StreamReader(stream))
+                {
+                    var result = JsonConvert.DeserializeObject<Dictionary<string, List<Dictionary<string, List<QuerySeries>>>>>(sr.ReadToEnd());
+                    return result["results"][0]["series"][0].Values;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ test/LightweightInfluxDb.Test/Tests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using LightweightInfluxDb;
+
+namespace LightweightInfluxDb.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void CanWrite()
+        {
+            // TODO
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void CanRead()
+        {
+            var idb = new InfluxDb(
+                "secret",
+                "secret",
+                "secret",
+                "secret");
+
+            var tsk = idb.QuerySingleSeries("show measurements");
+            tsk.Wait();
+            var r = tsk.Result;
+            Assert.True(tsk.Result.Count > 0);
+
+        }
+    }
+}
